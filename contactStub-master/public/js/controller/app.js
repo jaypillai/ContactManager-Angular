fb3c18e259b6ce456f431c7cbd1e342c2983e435
jaypillai <jkr8827@gmail.com>
2015-12-03 19:41:46 +0530

    testing

--- contactStub-master/public/js/controller/app.js
+++ contactStub-master/public/js/controller/app.js
@@ -1,64 +1,45 @@
-contactStubApp.controller("contactStubController", function ($scope, contactStubFactory, $location) {
+contactStubApp.controller("contactStubController", function ($scope, contactStubFactory, $location,$rootScope) {
     $scope.headerSrc = "tmpl/header.html";
 
-    $scope.contacts = contactStubFactory.query();
-
+    if (!$rootScope.contacts){
+        $rootScope.contacts = contactStubFactory.query();
+    } else {
+        $rootScope.contacts.forEach(function (val) {
+            if ($rootScope.currcontact && val.id == $rootScope.currcontact.id) {
+                
+                val.name = $rootScope.currcontact.name;
+                val.tel = $rootScope.currcontact.tel;
+      
+            }
+        });
+    }
+    
     $scope.currcontact = null;
 
-    $scope.getcontactById = function (id) {
-        var contacts = $scope.contacts;
-        for (var i = 0; i < contacts.length; i++) {
-            var contact = $scope.contacts[i];
-            if (contact.id == id) {
-                $scope.currcontact = contact;
-            }
-        }
+    $scope.deleteContact = function (contact) {
+        var index = $scope.contacts.indexOf(contact);
+        $scope.contacts.splice(index, 1);
     };
 
+    $scope.editContact = function (contact) {
+        $rootScope.currcontact = contact;
+        $location.path('/editContact');
+    };
+
+    $scope.addContact = function (contact) {
+        $rootScope.currcontact = contact;
+        $location.path('/addContact');
+    };
+    
+});
+
+contactStubApp.controller("editContactController", function ($scope, $location, $rootScope) {
+    $scope.currentUser = $rootScope.currcontact;
+    $scope.submitForm = function () {
+        $rootScope.currcontact = $scope.currentUser;
+        $location.path("/")
+    }
     $scope.back = function () {
-        window.history.back();
-    };
-
-    $scope.getCount = function (n) {
-        return new Array(n);
+        $location.path("/");
     }
-
-    $scope.isActive = function (route) {
-        return route === $location.path();
-    }
-
-    $scope.isActivePath = function (route) {
-        return ($location.path()).indexOf(route) >= 0;
-    }
-
-});
-
-contactStubApp.controller("contactDetailsController", function ($scope, $routeParams) {
-    $scope.getcontactById($routeParams.id);
-});
-contactStubApp.controller("bookTicketsController", function ($scope, $http, $location, $routeParams) {
-    $scope.getcontactById($routeParams.id);
-    $scope.onlyNumbers = /^\d+$/;
-    $scope.formData = {};
-    $scope.formData.contact_id = $scope.currcontact.id;
-    $scope.formData.contact_name = $scope.currcontact.name;
-    $scope.formData.date = "Today"
-
-    $scope.processForm = function () {
-        console.log($scope.formData);
-        $http({
-            method: 'POST',
-            url: '/book',
-            data: $.param($scope.formData), // pass in data as strings
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            } // set the headers so angular passing info as form data (not request payload)
-        })
-            .success(function (data) {
-                $location.path("/bookings");
-            });
-    };
-});
-contactStubApp.controller("bookingDetailsController", function ($scope, contactStubBookingsFactory) {
-    $scope.bookings = contactStubBookingsFactory.query();
 });
